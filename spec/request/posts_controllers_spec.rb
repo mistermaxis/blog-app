d97b519468a8dfdbedd519662a61b70f1bd86579
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-04-26 12:01:29 -0300

    Update tests

--- spec/request/posts_controllers_spec.rb
+++ spec/request/posts_controllers_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'PostsControllers', type: :request do
     it 'loads the lists of posts' do
       get user_posts_path(1)
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'This is a list of all the posts'
+      expect(response.body).to include 'List of Posts for User'
     end
     it 'loads each individual post' do
       get user_post_path(1, 1)
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'This a singular post'
+      expect(response.body).to include 'Post 1 for User 1'
     end
   end
 end
--- spec/request/users_controllers_spec.rb
+++ spec/request/users_controllers_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'UsersControllers', type: :request do
     it 'loads the list of users' do
       get users_path
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'This is a list of all the users'
+      expect(response.body).to include 'List of Users'
     end
     it 'loads each individual user' do
       get user_path(1)
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'This is a singular user'
+      expect(response.body).to include 'User'
     end
   end
 end
