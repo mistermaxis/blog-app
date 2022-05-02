13e721ab14c5b0796ca7879b5fac4ebb20998ddf
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 14:00:52 -0300

    Fix rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
   after_save :increase_comment_counter
-  
+
   def increase_comment_counter
     post = Post.find(post_id)
     post.increment!(:comments_counter)
