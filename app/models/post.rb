0a5a91d3013bb7859904a31c5ff078011eee7548
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 13:22:42 -0300

    Add increase post and recent comments method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :comments, class_name: "Comment", foreign_key: "post_id"
   has_many :likes, class_name: "like", foreign_key: "post_id"
+
+  def increase_post_counter
+    user = User.find(user_id)
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
