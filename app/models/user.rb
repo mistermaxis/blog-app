5597390949e9c3a1322006a3ede3de66b0ed5a8f
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 13:22:59 -0300

    Add recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts, class_name: "Post", foreign_key: "user_id"
-    has_many :comments, class_name: "Comment", foreign_key: "user_id"
-    has_many :likes, class_name: "Like", foreign_key: "user_id"
+  has_many :posts, class_name: "Post", foreign_key: "user_id"
+  has_many :comments, class_name: "Comment", foreign_key: "user_id"
+  has_many :likes, class_name: "Like", foreign_key: "user_id"
+  
+  def recent_posts
+      posts.order(created_at: :desc).limit(3) 
+  end
 end
