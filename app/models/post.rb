cf003d9da9656ab6b5b59f63355a816e47a75d13
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 16:27:53 -0300

    Change user_id to author_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
-  has_many :likes, class_name: 'like', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   after_save :increase_post_counter
 
   def increase_post_counter
