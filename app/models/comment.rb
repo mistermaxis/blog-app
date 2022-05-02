df9620607f67b9592307714cba7ad16d7c34b580
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 13:22:00 -0300

    Add comment increase method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   belongs_to :post
+
+  def increase_comment_counter
+    post = Post.find(post_id)
+    post.increment!(:comments_counter)
+  end
 end
