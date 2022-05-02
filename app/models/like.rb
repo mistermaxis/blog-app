668d05fa2836e4d1ca65bfcdb20d8c914a1b13cb
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-02 13:22:18 -0300

    Add like increase method

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   belongs_to :post
+
+  def increase_like_counter
+    post = Post.find(post_id)
+    post.increment!(:likes_counter)
+  end
 end
