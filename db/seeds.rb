6499862c763cd99b188fe4fe3b36b35aa320f122
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-04 11:38:22 -0300

    Add data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Users
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+# Posts
+first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author_id: first_user.id, title: 'Hello 2', text: 'This is my second post')
+third_post = Post.create(author_id: first_user.id, title: 'Hello 3', text: 'This is my third post')
+fourth_post = Post.create(author_id: first_user.id, title: 'Hello 4', text: 'This is my fourth post')
+fifth_comment = Post.create(author_id: first_user.id, title: 'Hello 5', text: 'This is my fifth post')
+
+# Comments
+first_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+second_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
+third_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
+fourth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
+fifth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
+sixth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
