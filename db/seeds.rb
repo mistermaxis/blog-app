ce0991922a1b6b04bf722002ca7ca4cd5fd97efd
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-04 11:56:09 -0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@ first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0Bx
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
 
 # Posts
-first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
-second_post = Post.create(author_id: first_user.id, title: 'Hello 2', text: 'This is my second post')
-third_post = Post.create(author_id: first_user.id, title: 'Hello 3', text: 'This is my third post')
-fourth_post = Post.create(author_id: first_user.id, title: 'Hello 4', text: 'This is my fourth post')
-fifth_comment = Post.create(author_id: first_user.id, title: 'Hello 5', text: 'This is my fifth post')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello 2', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello 3', text: 'This is my third post')
+fourth_post = Post.create(author: first_user, title: 'Hello 4', text: 'This is my fourth post')
+fifth_comment = Post.create(author: first_user, title: 'Hello 5', text: 'This is my fifth post')
 
 # Comments
-first_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
-second_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
-third_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
-fourth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
-fifth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
-sixth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
+first_comment = Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+second_comment = Comment.create(post: first_post, author: second_user, text: 'Hello Tom!' )
+third_comment = Comment.create(post: first_post, author: second_user, text: 'Nice Post Tom!' )
+fourth_comment = Comment.create(post: first_post, author: second_user, text: 'Hola Tom!' )
+fifth_comment = Comment.create(post: first_post, author: second_user, text: 'Ciao Tom!' )
+sixth_comment = Comment.create(post: first_post, author: second_user, text: 'Hallo Tom!' )
