3a95dc70ed28693b024c72b533f8920a549e565b
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-05 13:10:54 -0300

    Add comments and likes resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :index, :show, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
     end
   end
 end
