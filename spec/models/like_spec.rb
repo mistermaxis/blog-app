41c0a871670238e23d0cc881880dc7aa614ef943
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-06 17:33:20 -0300

    Add unit tests for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  author = User.first
+  
+  post = Post.first
+
+  subject { Like.new(author: author, post: post) } 
+
+  before(:each) { subject.save }
+
+  it 'Like belongs to correct author' do
+    expect(subject.author_id).to be(author.id)
+  end
+
+  it 'Like belongs to correct post' do
+    expect(subject.post_id).to be(post.id)
+  end
+
+  it 'Like counter is incremented in the post' do
+    incremented = post.likes_counter + 1
+    post.increment!(:likes_counter)
+    expect(subject.post.likes_counter).to be(incremented)
+  end
 end
