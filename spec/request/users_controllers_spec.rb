85bf88bc84f124ca4896b834c2e1dae9d1ab19f9
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-06 17:32:36 -0300

    Update users_controllers_spec

--- spec/request/users_controllers_spec.rb
+++ spec/request/users_controllers_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'UsersControllers', type: :request do
     it 'loads the list of users' do
       get users_path
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'List of Users'
+      expect(response.body).to include 'Tom'
     end
     it 'loads each individual user' do
       get user_path(1)
       expect(response).to have_http_status(200)
-      expect(response.body).to include 'User'
+      expect(response.body).to include 'Teacher from Mexico'
     end
   end
 end
