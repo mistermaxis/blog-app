db42297d2ce72645b1149ef87cf7095e5851bff7
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-06 17:30:42 -0300

    Add flash to LikesController create method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,11 +5,11 @@ class LikesController < ApplicationController
 
     @like = @post.likes.new(author: @user)
 
-    if @like.save
-      flash[:notice] = 'Like!'
-      redirect_to user_post_path(@post.author, @post)
-    else
-      flash[:notice] = 'Not liked.'
-    end
+    flash[:notice] = if @like.save
+                       'Liked!'
+                     else
+                       'Not liked.'
+                     end
+    redirect_to user_post_path(@post.author, @post)
   end
 end
