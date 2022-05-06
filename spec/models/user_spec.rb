fd5323c23bb4b7152ca02fae948a13e57a55eadf
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-06 17:32:53 -0300

    Add unit tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Test', photo: 'photo.jpg', bio: 'Test bio.') }
+
+  before(:each) { subject.save }
+
+  it 'Name must not be blank' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'PostsCounter must be an integer greater than or equal to zero.' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
 end
