5d5e5335129f4a51a7ece1fcaa797340f235286b
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-18 12:47:45 -0300

    Add delete comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = Comment.new(params.require(:comment).permit(:text))
+    @comment = @post.comments.new(params.require(:comment).permit(:text))
     @comment.author = current_user
     @comment.post = @post
 
@@ -11,5 +11,17 @@ class CommentsController < ApplicationController
                        'Comment was not created.'
                      end
     redirect_to user_post_path(@post.author, @post)
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:post_id])
+    @user = User.find_by_id(params[:user_id])
+    @comment = @post.comments.find(params[:format])
+    if @comment.destroy
+      flash[:success] = 'Comment destroyed successfully'
+    else
+      flash[:error] = 'Error: Comment could not be destroyed'
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
